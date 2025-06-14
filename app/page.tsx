2d6cbbff13a04069c53633b8504a35442d2a4d8e
Meetkumar Patel <71310616+meetpatel1812@users.noreply.github.com>
2025-06-14 00:09:00 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,17 +17,6 @@ export default async function Home() {
 
 
 
-<div style="position: relative; width: 100%; height: 0; padding-top: 50.0000%;
- padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
- border-radius: 8px; will-change: transform;">
-  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-    src="https://www.canva.com/design/DAGqTAcyU_c/zt87CL4_KyrgBBa9KUrZnQ/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-  </iframe>
-</div>
-<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGqTAcyU_c&#x2F;zt87CL4_KyrgBBa9KUrZnQ&#x2F;view?utm_content=DAGqTAcyU_c&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Design</a> by 19BECE3O112 Meet
-
-        
-        
 
 {/* old code */}
         <div className="absolute inset-0 bg-black/40 z-10"></div>
