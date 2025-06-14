863e4f80b722f72f6845aa2fdbcb637a39beaf3e
Meetkumar Patel <71310616+meetpatel1812@users.noreply.github.com>
2025-06-14 00:03:00 -0400

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
               Authentic Indian cuisine in Sherwood Park, Alberta. Experience the rich flavors of India.
             </p>
             <div className="flex space-x-4">
-              <Link href="#" className="text-gray-600 hover:text-red-600">
+              <Link href="https://www.facebook.com/profile.php?id=61577046339862&rdid=w5tTFOPTIKikgqoJ&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1As5GGk48s%2F#" className="text-gray-600 hover:text-red-600">
                 <span className="sr-only">Facebook</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -31,7 +31,7 @@ export default function Footer() {
                   <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                 </svg>
               </Link>
-              <Link href="#" className="text-gray-600 hover:text-red-600">
+              <Link href="https://www.instagram.com/curryinhurry_sherwood_park?igsh=MWc4YW1wM3J2bzltdQ%3D%3D" className="text-gray-600 hover:text-red-600">
                 <span className="sr-only">Instagram</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
